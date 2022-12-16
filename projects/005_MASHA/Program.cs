489dc2bebdf1431319ce43d8d5a6caca3933c048
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-16 14:17:07 +0300

    Project 005 MASHA was created

---
+++ projects/005_MASHA/Program.cs
@@ -0,0 +1,14 @@
+﻿// Как приветствовать пользователя-любимчика
+Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+// if(username == "Маша")
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
