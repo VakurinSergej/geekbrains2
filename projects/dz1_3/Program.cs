7f06063aaa3b67cc944052f408616f66d29dbbf4
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-16 20:38:51 +0300

    DZ1 task 3

---
+++ projects/dz1_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти максимальное из трех чисел
+
+
+int a = 11;
+int b = 538;
+int c = 7;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max =  ");
+Console.WriteLine(max);
+
