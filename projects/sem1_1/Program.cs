afda8cdf9777d118ba18d4356fe6d945df875c8c
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-16 18:36:38 +0300

    Sem1 task1

---
+++ projects/sem1_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача показать квадрат числа
+
+int a = new Random().Next(1,15);
+Console.Write("выбрано число: ");
+Console.WriteLine(a);
+Console.Write("квадрат числа: ");
+Console.WriteLine(a*a);
+
