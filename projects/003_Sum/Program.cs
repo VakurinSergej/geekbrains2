e8712728cace154e8bcce633bdfaff4ffd0e279c
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-16 13:27:46 +0300

    Project 003 Sum was created

---
+++ projects/003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// сложение двух чисел
+int numberA = 30;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+// строка 3 Console.WhriteLine(numberA + numberB); - конец программы
