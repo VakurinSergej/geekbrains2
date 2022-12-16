b3bca576f78cba40c46f89481fdf2934ca457ef4
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-16 18:52:46 +0300

    second option was added

--- projects/sem1_1/Program.cs
+++ projects/sem1_1/Program.cs
@@ -1,8 +1,15 @@
 ﻿// Задача показать квадрат числа
 
-int a = new Random().Next(1,15);
-Console.Write("выбрано число: ");
-Console.WriteLine(a);
-Console.Write("квадрат числа: ");
-Console.WriteLine(a*a);
+// вариант1
+// int a = new Random().Next(1,15);
+// Console.Write("выбрано число: ");
+// Console.WriteLine(a);
+// Console.Write("квадрат числа: ");
+// Console.WriteLine(a*a);
 
+// вариант2
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int result = a*a;
+Console.Write("квадрат числа = ");
+Console.WriteLine(a*a);
