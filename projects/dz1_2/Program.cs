7c5f285e597e2b94dadf91c58a8d86a89de5e714
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-16 20:13:18 +0300

    DZ1 task 2

---
+++ projects/dz1_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Введите число от 1 до 7: ");
+string username = Console.ReadLine();
+
+if (username== "1")
+{
+    Console.WriteLine("Сегодня ПОНЕДЕЛЬНИК");
+}
+if(username == "2")
+{
+    Console.WriteLine("Сегодня ВТОРНИК");
+}
+if(username == "3")
+{
+    Console.WriteLine("Сегодня СРЕДА");
+}
+if(username == "4")
+{
+    Console.WriteLine("Сегодня ЧЕТВЕРГ");
+}
+if(username == "5")
+{
+    Console.WriteLine("Сегодня ПЯТНИЦА");
+}
+if(username == "6")
+{
+    Console.WriteLine("Сегодня СУББОТА");
+}
+if(username == "7")
+{
+    Console.WriteLine("Сегодня ВОСКРЕСЕНЬЕ");
+}
