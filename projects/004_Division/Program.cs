2290e0ade6e2ccc276a10ae3c48d9a648d6d49aa
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-16 13:42:18 +0300

    Project 004 Division was created

---
+++ projects/004_Division/Program.cs
@@ -0,0 +1,9 @@
+﻿// Деление двух чисел
+
+// int numberA = 10;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+double numberA = 17;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
