e22658f41695fc755dc3c8c6f410872a71000f95
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-16 17:27:43 +0300

    Project 007 Max Number was created

---
+++ projects/006_MaxNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Находим максимальное число (задача с гирями)
+int a = 11;
+int b = 8;
+int c = 5;
+int d = 3;
+int e = 6;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max =  ");
+Console.WriteLine(max);
