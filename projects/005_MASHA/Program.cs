85cb392e6cffd69dc28151c0ef2ada239e3adc57
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-17 22:34:05 +0300

    add ToLower

--- projects/005_MASHA/Program.cs
+++ projects/005_MASHA/Program.cs
@@ -1,10 +1,12 @@
 ﻿// Как приветствовать пользователя-любимчика
-Console.Write("Введите имя пользователя ");
+
+Console.Write("Введите ваше имя: ");
 string username = Console.ReadLine();
 // if(username == "Маша")
+// if(username.ToLower() == "Маша")
 if(username.ToLower() == "Маша")
 {
-    Console.WriteLine("Ура, это же Маша!");
+    Console.WriteLine("Ура, это же МАША!");
 }
 else
 {
