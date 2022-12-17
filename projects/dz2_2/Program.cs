c9ed4bd382424a2044bb1a364ccc91b722a6e743
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-17 16:03:38 +0300

    DZ2 task 3

---
+++ projects/dz2_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a > 100)
+{
+int b = (a % 10);
+Console.Write("Третья цифра числа: ");
+Console.WriteLine(b);
+}
+if (a < 100)
+{
+Console.Write("Третьей цифры нет");
+}
+// if (a > 1000)
+// {
+// break
+// int b = (a % 10);
+// Console.Write("Третья цифра числа: ");
+// Console.WriteLine(b);
+// }
