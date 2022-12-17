ea0cc43d4eba6ce0cc1c9e006da2bf5d76024b78
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-17 22:10:29 +0300

    Seminar 2 Task 2

---
+++ projects/Sem2_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23
+
+// Вариант 1
+// Console.WriteLine("Введите число: ");
+// var entNumber = Convert.ToInt32(Console.ReadLine());
+// var firstPer = 7;
+// var secondPer = 23;
+// if (entNumber % firstPer == 0 && entNumber % secondPer == 0)
+// {
+//     Console.Write("Введенное число кратно числам: ");
+//     Console.WriteLine(firstPer);
+//     Console.Write(" и ");
+//     Console.WriteLine(secondPer);
+// }
+// else
+// {
+//    Console.WriteLine("Введенное число не кратно заданным числам.");
+// } 
+
+// Вариант 2
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if ( a%7 == 0 && a%23 == 0 )
+{
+    Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Число НЕ кратно 7 и 23");
+}
