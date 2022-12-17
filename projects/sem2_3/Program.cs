8bc42438d5c85c4970c931a5f1ed5afe80015322
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-17 22:40:15 +0300

    chanches were added

--- projects/sem2_3/Program.cs
+++ projects/sem2_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int N1 = (N) * (-1);
+
+ while (N1 <= N)
+{
+    Console.WriteLine(N1);
+    N1++;
+
+}
