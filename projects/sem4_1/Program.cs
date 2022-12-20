5d1e9c9b13820b5fce1196b106aae6bdc217d0fc
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-20 23:08:08 +0300

    Seminar 4 Task 1

---
+++ projects/sem4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N 
+// и выдаёт сумму чисел от 1 до N
+
+// вариант 1 - математический
+
+// Console.Write("-> ");
+// double N = double.Parse(Console.ReadLine()!);
+// Console.Write((1 + N) * (N / 2));
+
+// вариант 2 - более простой
+Console.Write("Введите число N ");
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i=1; i<=N; i++) 
+{
+sum=sum+i;
+}
+Console.WriteLine(sum);
