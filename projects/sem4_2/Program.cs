5038355001e8aafa52813327962baadd2688a134
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-20 23:14:37 +0300

    Seminar 4 Task 2

---
+++ projects/sem4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N
+
+Console.Write("Введите число N ");
+int N = int.Parse(Console.ReadLine());
+
+int GetMultiply (int a)
+{
+    int multiply = 1;  
+    for (int i=1; i<=a; i++) multiply=multiply*i;
+    return multiply;
+}
+
+Console.WriteLine(GetMultiply(N));
