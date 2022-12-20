30deeae0a1f40cd18c86f5b6c96cb7c2d2cbb215
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-20 22:37:23 +0300

    DZ3 task 1 - havent finished yet

---
+++ projects/dz3_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом
+// (читается слева направо и справа налево одинаково)
+
+Console.WriteLine("Введите число: ");
+string number = int.Parse(Console.ReadLine());
+for (i=0; i < number.Length; i++;)
+{
+if (number[i] == number[number.Length])
+    (i++, number.Length--);
+    Console.WriteLine("это число полиндронно");
+//else
+  //  Console.WriteLine("это число НЕ полиндронно");
+}
