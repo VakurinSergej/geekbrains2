42df372c18e998fbe8f32e3a4ec503915cc363b1
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-21 22:20:25 +0300

    DZ4 Task1 - doesnt work

---
+++ projects/dz4_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. Без использования Math.Pow
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int result = 0;
+int n = 1;
+if (n <= b)
+{
+    int result = a*a;
+    n++;
+}
+Console.WriteLine(result);
+
