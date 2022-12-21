ef64f4a41be54e50f9fb512588e5e9cfc29c1183
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-21 22:21:02 +0300

    DZ4 Task2 - dosent work

---
+++ projects/dz4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int n = number.Length;
+int sum = 0;
+
+for (int i=0; i<n; i++) 
+{
+     sum = sum + number[i];
+}
+Console.WriteLine(sum);
+
+
