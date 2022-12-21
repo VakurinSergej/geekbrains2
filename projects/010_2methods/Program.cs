9003f19b56ed4fd7efce3471b1e1bf52c6d7399e
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-21 13:00:17 +0300

    Lesson 2 Task 3 - 2 Methods

---
+++ projects/010_2methods/Program.cs
@@ -0,0 +1,34 @@
+﻿// Просим показать все элементы массива по порядку
+// для этого создает 2 метода: 
+// 1) заполнения массива случайными числами и 
+// 2) печать массива в консоль
+
+// метод 1 - заполняет массив случайными числами
+void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10); // заполняет случайными числами от 1 до 10
+        index++;
+    }
+}
+
+// метод 2 - выводит на печать
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int [10]; // новый массив с 10 элементами
+FillArray(array);
+PrintArray(array);
+
