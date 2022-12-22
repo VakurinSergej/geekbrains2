14183f50b504a37c9503574f1d84af6e4ace65b9
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-22 22:38:03 +0300

    Seminar 5 Task 3

---
+++ projects/sem5_3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте массив из 8 случайных чисел из промежутка [-9, 9]. 
+// Напишите программу получает на вход число и определяет, 
+// присутствует ли заданное число в массиве.
+
+
+Cvoid FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-9, 9);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(coll[pos] + " ");
+        pos++;
+    }
+
+}
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(" ");
+
+Console.WriteLine("Введите число: ");
+int zChislo = Convert.ToInt32(Console.ReadLine());
+int chisloEst = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == zChislo)
+    {
+        chisloEst++;
+    }
+}
+if (chisloEst > 0)
+{
+    Console.WriteLine($"Заданное число присутствует в массиве в количестве {chisloEst}.");
+}
+else
+{
+    Console.WriteLine("Заданного числа в массиве нет.");
+}
+
+
