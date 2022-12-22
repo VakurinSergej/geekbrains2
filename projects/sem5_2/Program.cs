0fcbc2fe3894ff1da4bbbbf30f1be141f4c5f72d
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-22 22:36:00 +0300

    Seminar 5 Task 2

---
+++ projects/sem5_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив из 10 элементов из промежутка [-9, 9] 
+// и положительные элементы замените на соответствующие отрицательные, и наоборот
+
+int[] array = new int [10];
+
+Console.Write("[");
+for (int i = 0; i <array.Length; i++)
+
+{
+    int randomNumber = new Random().Next(-9, 10);
+    array [i] = randomNumber;
+    Console.Write(array [i].ToString() + ", ");
+    
+}
+Console.Write("] -> ");
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = (array [i] * -1);
+    Console.Write(array [i].ToString() + ", ");
+}
+Console.Write("]");
+Console.WriteLine();
+
+
+
