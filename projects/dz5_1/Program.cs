d4dc8d7ee074a0108105d3fe9e410161a2d5f9d1
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-24 01:33:20 +0300

    DZ 5 Task 1 done

---
+++ projects/dz5_1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(coll[pos] + " ");
+        pos++;
+    }
+
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(" ");
+int count = 0;
+
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i] % 2 == 0) 
+    {
+        count++;
+    }
+}
+ Console.WriteLine("Четных чисел: " + count);
+
