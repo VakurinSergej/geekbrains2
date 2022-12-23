3e21b71aa859624c45d85b74a0d3a7d64326e42d
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-23 09:35:14 +0300

    DZ4 Task 1

--- projects/dz4_1/Program.cs
+++ projects/dz4_1/Program.cs
@@ -5,12 +5,13 @@ Console.WriteLine("Введите первое число: ");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int b = int.Parse(Console.ReadLine());
-int result = 0;
+int result = 1;
 int n = 1;
 while (n <= b)
 {
-    int result = a*a;
+    result = result*a;
     n++;
 }
 Console.WriteLine(result);
+
 
