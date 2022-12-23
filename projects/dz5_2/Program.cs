3232e90bb0b354852245d5192af5ca60e24534bb
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-24 01:58:40 +0300

    DZ 5 Task 2 done

---
+++ projects/dz5_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 11);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(coll[pos] + " ");
+        pos++;
+    }
+
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(" ");
+
+// //////// это сумма нечетных элементов
+// int sum = 0;
+
+// for (int i=0; i<array.Length; i++)
+// {
+//     if (!(array[i] % 2 == 0)) 
+//     {
+//         sum = sum + array[i];
+//     }
+// }
+//  Console.WriteLine("Сумма нечетных чисел: " + sum);
+
+int sum = 0;
+
+for (int i=0; i<array.Length; i++)
+{    
+     if (!(i % 2 == 0)) 
+     {
+        sum = sum + array[i];
+     } 
+}
+ Console.WriteLine("Сумма чисел нечетных позиций: " + sum);
+
+
