d2877115bf6319295bb00a1a9815b854d7621e80
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-23 23:09:01 +0300

    Seminar 6 Task 1

---
+++ projects/sem6_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.). 
+// Элементы одномерного массива задаются случайно и лежат в промежутке от -10 до 10
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-10, 11);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(coll[pos] + " ");
+        pos++;
+    }
+
+}
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(" ");
+int[] newarray = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    newarray[i] = array[array.Length - 1 - i];
+     Console.Write(newarray[i] + " ");
+}
+
