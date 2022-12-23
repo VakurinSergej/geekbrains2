282dc5351d06377dcda10632106f52f2110b287d
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-24 02:14:11 +0300

    DZ 5 Task 3 done accordind seminar6

---
+++ projects/dz5_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+
+double[] array = new double[5];
+for (int i = 0; i < array.Length; i++)
+{
+    double number = new Random().Next(0, 10) + new Random().NextDouble(); 
+    array[i] = Math.Round(number, 2); 
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+double min = array[0];
+double max = array[0];
