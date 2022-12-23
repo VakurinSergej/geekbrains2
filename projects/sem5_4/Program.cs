cfeaab9b3983b5312da1d3f7b8fb9b0feea01a1f
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-23 09:27:04 +0300

    Seminar 5 Task 4

---
+++ projects/sem5_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив из 10 случайных чисел из промежутка [-100, 100]. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]
+
+
+void ArrayPrint (int[] a)
+{
+Console.Write("[");
+for (int i=0; i<a.Length-1; i++) Console.Write($"{a[i]}, ");
+Console.WriteLine($"{a[a.Length-1]}]");
+}
+int[] Array =new int[10];
+int count = 0;
+
+for (int i=0; i<Array.Length; i++) 
+{
+Array[i] = new Random().Next(-100,101);
+}
+ArrayPrint(Array);
+Console.WriteLine("Найдем количество элементов массива из отрезка [10;99]");
+
+for (int i=0; i<Array.Length; i++)
+{
+    if (Array[i]>=10&&Array[i]<=99) 
+    {count +=1;
+    Console.WriteLine(Array[i]);
+    }
+   
+}
+ Console.WriteLine("Таких элементов в массиве:" + count);
