ddaee5434171a807139e0df9fd043592912928fd
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-24 16:00:35 +0300

    DZ 5 Task 3 done

--- projects/dz5_3/Program.cs
+++ projects/dz5_3/Program.cs
@@ -15,3 +15,26 @@ for (int i = 0; i < array.Length; i++)
 
 double min = array[0];
 double max = array[0];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] > max)
+        {
+            max = array[i];
+        }
+if (array[i] < min)
+        {
+            min = array[i];
+        }
+
+}
+
+Console.WriteLine($"Максимальное значение: {max}, минимальное значение: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением: {max - min}");
+
+
+
+
+
+
