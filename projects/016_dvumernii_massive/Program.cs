1dc497b96ab1e364c303d64c3555aa70ee4c2197
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-25 21:54:50 +0300

    Lesson 4 Code string + matrix

---
+++ projects/016_dvumernii_massive/Program.cs
@@ -0,0 +1,24 @@
+﻿// Двумерный массив
+
+// // Массив как сетка строк и столбцов, пустая или заполненная текстом
+// string[,] table = new string [2,5]; // [2,5] - 2 строки 5 столбцов
+// table[1,2] = "слово"; // в клетке со строкой 1 и столбцом 2 лежит слово
+// for (int rows = 0; rows < 2; rows++) // 2 - кол-во строк = length
+// {
+//     for (int columns = 0; columns < 5; columns++) // 5 = length
+//     {
+//         Console.WriteLine($"-{table [rows,columns]}-");
+//     }
+// }
+
+// Массив как числовая сетка
+int [,] matrix = new int [3,4]; // 3 - кол-во строк, 4 - кол-во столбцов
+for (int rows = 0; rows < 3; rows++) // 3 = matrix.GetLength(0) - 0 - это строки
+{
+    for (int columns = 0; columns < 4; columns++) // 4 = matrix.GetLength(1) - 1 - это столбцы
+    {
+        Console.WriteLine($"{matrix[rows,columns]}");
+    }
+    Console.WriteLine();
+}
+
