99ca8197b8f31206f6dbd98de0a7cab4c78e8e6d
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-25 11:43:27 +0300

    DZ 6 Task 2

---
+++ projects/dz6_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+Console.WriteLine("Введите первое число: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите четвертое число: ");
+double k2 = double.Parse(Console.ReadLine());
+
+double x = ((b2-b1)/(k1-k2));
+Console.WriteLine($"x равен" + x);
+
+double y1 = ((k1*x)+b1);
+double y2 = ((k2*x)+b2);
+Console.WriteLine($"Координаты точки пересечения двух прямы:" + (y1, y2));
+
