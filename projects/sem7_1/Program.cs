a3769c6be61c500bf8e79201be5a8eac9efd7693
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-26 22:21:31 +0300

    Seminar 7 Task 1

---
+++ projects/sem7_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: 
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+
+// 0 1 2 3
+
+// 1 2 3 4
+
+// 2 3 4 5
+int m = new Random().Next(3,6);
+int n = new Random().Next(3,6);
+int[,] arr = new int[n,m]; // [количество строк, количество столбцов]
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++) 
+    {
+        arr[i, j] = i+j; 
+    }
+}
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine(); 
+}
+
