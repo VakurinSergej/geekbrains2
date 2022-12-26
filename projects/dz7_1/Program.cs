06649ce67dc290cbfca633ca513fb2568ca67119
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-26 23:43:26 +0300

    DZ 7 Task 1 (47) - void FillArray - doesnt work

---
+++ projects/dz7_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами от -10,0 до 10,0.
+
+Console.WriteLine("Введите число строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число столбцов: ");
+int n = int.Parse(Console.ReadLine());
+int [,] matrix = new int [m, n];
+
+void PrintArray (int[,] matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[,] matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(-10, 10) + new Random().NextDouble();
+        }
+    }
+}
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+
+
+
+
+
+
+
