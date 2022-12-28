9ac9c25692bac99bbae7df75a942cb7f75b89a39
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-28 21:04:18 +0300

    Lesson 1 Task 5

--- projects/005_MASHA/Program.cs
+++ projects/005_MASHA/Program.cs
@@ -2,9 +2,9 @@
 
 Console.Write("Введите ваше имя: ");
 string username = Console.ReadLine();
+
 // if(username == "Маша")
-// if(username.ToLower() == "Маша")
-if(username.ToLower() == "Маша")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же МАША!");
 }
