0f452a5cd827173b4537e1ca907e82bbba0519be
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-28 21:01:14 +0300

    Lesson 1 Task 4

--- projects/004_Division/Program.cs
+++ projects/004_Division/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Деление двух чисел
 
+//// вариант 1
 // int numberA = 10;
 // int numberB = 5;
 // Console.WriteLine(numberA / numberB);
 
+// вариант 2
 double numberA = 17;
 double numberB = 5;
 Console.WriteLine(numberA / numberB);
