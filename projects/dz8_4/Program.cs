37318db192f01f97fd160d5afb19e0a8457d9efe
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-29 01:15:21 +0300

    DZ8 Task 4 not finished yet

--- projects/dz8_4/Program.cs
+++ projects/dz8_4/Program.cs
@@ -2,3 +2,17 @@
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 
+
+int [,,] array3d = new int [2,2,2];
+for (int i = 0; i < array3d.GetLength(0); i++)
+{
+    for (int j = 0; j < array3d.GetLength(1); j++)
+    {
+        for (int k = 0; k < array3d.GetLength(2); k++)
+        {
+            Console.WriteLine(array3d[i,j,k]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
