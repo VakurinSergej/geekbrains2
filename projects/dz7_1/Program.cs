5fd8dc239238db8628acf766f4b51bc90ffc5da0
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-28 19:05:21 +0300

    DZ 7 Task 1 done

--- projects/dz7_1/Program.cs
+++ projects/dz7_1/Program.cs
@@ -5,9 +5,24 @@ Console.WriteLine("Введите число строк: ");
 int m = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число столбцов: ");
 int n = int.Parse(Console.ReadLine());
-int [,] matrix = new int [m, n];
 
-void PrintArray (int[,] matr)
+double [,] matrix = new double [m, n];
+
+void FillArray (double[,] matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            double number = new Random().Next(-10, 10) + new Random().NextDouble();
+            number = Math.Round (number, 3);
+            matr[i,j] = number;
+
+        }
+    }
+}
+
+void PrintArray (double[,] matr)
 {
     for (int i=0; i<matr.GetLength(0); i++)
     {
@@ -19,25 +34,10 @@ void PrintArray (int[,] matr)
     }
 }
 
-void FillArray (int[,] matr)
-{
-    for (int i=0; i<matr.GetLength(0); i++)
-    {
-        for (int j=0; j<matr.GetLength(1); j++)
-        {
-            matr[i,j] = new Random().Next(-10, 10) + new Random().NextDouble();
-        }
-    }
-}
-
 PrintArray(matrix);
 FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
-
-
-
-
 
 
 
