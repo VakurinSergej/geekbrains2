12204dfb388bec748e6e4310d5aa611f8f7f87d4
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-29 23:39:29 +0300

    DZ 8 Task 3 = Task 58

--- projects/dz8_3/Program.cs
+++ projects/dz8_3/Program.cs
@@ -22,31 +22,50 @@ void FillArray (int[, ]matr)
     {
         for (int j=0; j<matr.GetLength(1); j++)
         {
-            matr[i,j] = new Random().Next(0,11);
+            matr[i,j] = new Random().Next(0,10);
         }
     }
 }
 
-int [, ] matrix = new int [5, 2];
-int [, ] matrix2 = new int [5, 2];
-// int [, ] matrix3 = new int [5, 2];
+int [, ] matrix1 = new int [2, 2];
+int [, ] matrix2 = new int [2, 2];
+int [, ] matrix3Result = new int [2, 2];
 
+FillArray(matrix1);
+Console.WriteLine();
+PrintArray(matrix1);
+Console.WriteLine();
 
-FillArray(matrix);
 FillArray(matrix2);
-PrintArray(matrix);
 Console.WriteLine();
 PrintArray(matrix2);
+Console.WriteLine();
 
 
-// for (int i = 0; i < matrix.GetLength(0); i++)
-// {
-//     for (int j = 0; j < matrix.GetLength(1); j++)
-//     {
-//         {
-//             int[,] matrix3 = matrix[i,j] * matrix2[i,j];
-//             Console.WriteLine(matrix3);
-//         }
-//     }
-// }
+void MultiplyMatrix(int[,] matrix1, int[,] matrix2, int[,] matrix3Result)
+{
+    for (int i = 0; i < matrix3Result.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3Result.GetLength(1); j++)
+        {
+           int multiply = 0;
+           for (int k = 0; k < matrix1.GetLength(1); k++)
+           {   
+              multiply = matrix1[i,j] * matrix2[i,j];
+           }
+           matrix3Result[i,j] = multiply;
+        }
+    }
+}
 
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+
+MultiplyMatrix(matrix1, matrix2, matrix3Result);
+Console.WriteLine($"\nПроизведение первого и второго массива:");
+PrintArray(matrix3Result);
