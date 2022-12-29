82e187b2f6424e3206ddb7e10d541967c71f23b5
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-30 00:18:43 +0300

    DZ 8 Task 4 = Task 60

--- projects/dz8_4/Program.cs
+++ projects/dz8_4/Program.cs
@@ -2,6 +2,35 @@
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 
+void PrintArray (int[,,]matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            for (int k=0; k<matr.GetLength(2); k++)
+            {
+                Console.Write($"{matr[i, j, k]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[,,]matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+           for (int k=0; k<matr.GetLength(2); k++)
+           {
+                matr[i,j,k] = new Random().Next(0,10);
+           } 
+        }
+    }
+}
+
 
 int [,,] array3d = new int [2,2,2];
 for (int i = 0; i < array3d.GetLength(0); i++)
@@ -10,9 +39,18 @@ for (int i = 0; i < array3d.GetLength(0); i++)
     {
         for (int k = 0; k < array3d.GetLength(2); k++)
         {
-            Console.WriteLine(array3d[i,j,k]);
+            Console.Write($"{array3d[i, j, k]} ");
         }
-        Console.WriteLine();
     }
     Console.WriteLine();
 }
+    
+
+FillArray(array3d);
+Console.WriteLine();
+PrintArray(array3d);
+Console.WriteLine();
+
+
+
+
