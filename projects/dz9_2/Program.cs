50625e3d85743736b7cd25f6cde92564a7578140
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-30 00:27:04 +0300

    DZ 9 Task 2

---
+++ projects/dz9_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от 1 до N.
+
+
+
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumber(n, m));
+
+
+int PrintNumber(int n, int m)
+{
+    if (n == m) return n;
+    return (n + PrintNumber(n + 1, m));
+}
