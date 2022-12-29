e0d599ec5c90760ef1ae6d7a804c62c179a50352
YuliaOkhotnikova <120200912+YuliaOkhotnikova@users.noreply.github.com>
2022-12-29 22:52:02 +0300

    DZ 8 Task 1 = Task 54

--- projects/dz8_1/Program.cs
+++ projects/dz8_1/Program.cs
@@ -3,59 +3,6 @@
 // каждой строки двумерного массива
 
 
-// void PrintArray (int[,] matr)
-// {
-//     for (int i=0; i<matr.GetLength(0); i++)
-//     {
-//         for (int j=0; j<matr.GetLength(1); j++)
-//         {
-//             Console.Write($"{matr[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-
-// void FillArray (int[,] matr)
-// {
-//     for (int i=0; i<matr.GetLength(0); i++)
-//     {
-//         for (int j=0; j<matr.GetLength(1); j++)
-//         {
-//             matr[i,j] = new Random().Next(0,11);
-//         }
-//     }
-// }
-
-// void SelectionSort(int[,] matr)
-// {
-//     for (int i = 0; i < matr.GetLength(1); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(0); j++)
-//         {
-//             int minPositionI = 0;
-//             for (int k = j+1; k < matr.GetLength(0); k++) 
-//             {
-//                 if (matr[k] < matr[minPositionI])
-//                 minPositionI = k;
-//             }
-//             int temporary = matr[i];
-//             matr[i] = matr[minPositionI];
-//             matr[minPositionI] = temporary;
-//         }
-//     }
-// }
-
-// int [,] matrix = new int [5, 4];
-// PrintArray(matrix);
-// FillArray(matrix);
-// Console.Write();
-// // SelectionSort(matrix);
-// PrintArray(matrix);
-
-
-
-
-
 void PrintArray (int[, ]matr)
 {
     for (int i=0; i<matr.GetLength(0); i++)
@@ -81,23 +28,23 @@ void FillArray (int[, ]matr)
 
 void SelectionSort(int[,] matr)
 {
-    for (int i = 0; i < matr.GetLength(1); i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for (int j = 0; j < matr.GetLength(0)-1; j++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            int minPositionI = 0;
-            for (int k = j+1; k < matr.GetLength(0); k++) 
+            for (int k = 0; k < matr.GetLength(1)-1; k++) 
             {
-                if (matr[k,j] < matr[minPositionI,j])
-                minPositionI = k;
+                if (matr[i,k] < matr[i, k + 1])
+                {
+                    int temporary = matr[i, k + 1];
+                    matr[i, k + 1] = matr[i, k];
+                    matr[i,k] = temporary;
+                }
             }
-            int temporary = matr[i,j];
-            matr[i,j] = matr[minPositionI,j];
-            matr[minPositionI,j] = temporary;
+            
         }
     }
 }
-
 
 
 int [, ] matrix = new int [5, 5];
@@ -109,9 +56,4 @@ SelectionSort(matrix);
 Console.WriteLine();
 PrintArray(matrix);
 
-
-// for (int i = 0; i < matrix.GetLength(0); i++)
-// {
-//     for (int j = 0; j < matrix.GetLength(1); j++);
-// }
 
